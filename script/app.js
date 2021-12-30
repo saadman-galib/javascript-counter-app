2597d7fc3c1db0fa4a3b29075ebd1762587f8271
Md. Saadman Galib Rabbi <galib2829@gmail.com>
2021-12-31 01:16:05 +0600

    Initial commit

---
+++ script/app.js
@@ -0,0 +1,16 @@
+var countNum = document.getElementById("counter");
+var increase = document.getElementById("increase");
+var decrease = document.getElementById("decrease");
+
+var num = 0;
+
+countNum.innerHTML = num;
+
+increase.addEventListener("click", (a) => {
+    num += 1;
+    countNum.innerHTML = num;
+});
+decrease.addEventListener("click", (a) => {
+    num -= 1;
+    countNum.innerHTML = num;
+});
