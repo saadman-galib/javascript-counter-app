14b69b01f97b402499dd27a0f22b216274114a3b
Md. Saadman Galib Rabbi <galib2829@gmail.com>
2022-11-23 22:08:06 +0000

    Update app.js

--- script/app.js
+++ script/app.js
@@ -3,17 +3,19 @@ var increase = document.getElementById("increase");
 var decrease = document.getElementById("decrease");
 
 var num = 0;
-
 countNum.innerHTML = num;
 
 increase.addEventListener("click", (a) => {
     num += 1;
     countNum.innerHTML = num;
+    decrease.style.backgroundColor = '#44b3d7';
 });
 decrease.addEventListener("click", (a) => {
     num -= 1;
-    if (num<0){
-        num =0
+    if (num <= 0){
+        decrease.style.backgroundColor = "#6CA5B6";
+        num = 0
     }
+
     countNum.innerHTML = num;
 });
